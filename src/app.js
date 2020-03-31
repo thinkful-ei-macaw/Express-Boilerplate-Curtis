f0b5044b5dacde3ed9ce8ec5090102d49327ed52
curt Sessions <cjsesh2018@gmail.com>
2020-03-30 23:16:10 -0600

    final

---
+++ src/app.js
@@ -0,0 +1,42 @@
+require('dotenv').config()
+const express = require('express')
+const morgan = require('morgan')
+const cors = require('cors')
+const helmet = require('helmet')
+const { NODE_ENV } = require('./config')
+
+
+const app = express()
+
+const morganOption = (NODE_ENV === 'production')
+? 'tiny'
+: 'common';
+
+app.use(morgan(morganOption))
+app.use(express.json());
+app.use(helmet())
+app.use(cors())
+
+app.post('/', (req, res) => {
+console.log(req.body);
+res
+    .send('POST request received.');
+});
+
+app.get('/', (req, res) => {
+res
+    .send('A GET Request');
+});
+
+app.use(function errorHandler(error, req, res, next) {
+    let response
+    if (NODE_ENV === 'production') {
+        response = { error: { message: 'server error' } }
+    } else {
+        console.error(error)
+        response = { message: error.message, error }
+    }
+    res.status(500).json(response)
+    })
+
+module.exports = app
